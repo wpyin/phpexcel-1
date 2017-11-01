a332a6303f4be3307e81e59d179608681a448a20
wpyin <820344233@qq.com>
2017-11-01 11:51:52 +0800

    修改了demo 同时解决乱码问题
    在Git上获取的但有几个地方有些错误回头可以给他说下。

--- import.php
+++ import.php
@@ -1,11 +1,13 @@
 <?php
+header("content_type:text/html;charset=utf8");
  /** 引入需要的类库*/
 require_once 'Classes\phpexcel.php'; 
 require_once 'Classes\PHPExcel\IOFactory.php';
 require_once 'Classes\PHPExcel\Reader\Excel5.php'; 
 
-//数据库连接
+//数据库连接导入数据库里面
  $link = mysqli_connect('localhost','root','','test');
+ mysqli_set_charset($link,"utf8");//不写这句话的话会造成乱码
  if(!$link){
 	 echo "数据库连接失败"; 
 	 exit;
@@ -13,7 +15,7 @@ require_once 'Classes\PHPExcel\Reader\Excel5.php';
  
 //读excel表中内容生成对应数组
 $objReader = PHPExcel_IOFactory::createReader('Excel2007');
-$objPHPExcel = $objReader->load('1.xlsx'); 
+$objPHPExcel = $objReader->load('shiyan.xlsx'); 
 
 //读取csv文档内容
 // $objReader = new PHPExcel_Reader_CSV();
@@ -37,23 +39,34 @@ for($j=1;$j<=$highestRow;$j++)
 //数组转换成字符串，方便直接插入数据库中
 $str = '';
  foreach($arr_result as $item){
-	$strs.=$item.',';
+	$str.=$item.',';
 } 
-
+//print_r($str);die;
 for($j=2;$j<=$highestRow;$j++)
 {
 	$a = $objPHPExcel->getActiveSheet()->getCell("A".$j)->getValue();//获取A列的值
 	$b = $objPHPExcel->getActiveSheet()->getCell("B".$j)->getValue();//获取B列的值
 	$c = $objPHPExcel->getActiveSheet()->getCell("C".$j)->getValue();
 	$d = $objPHPExcel->getActiveSheet()->getCell("D".$j)->getValue();
+	$e = $objPHPExcel->getActiveSheet()->getCell("E".$j)->getValue();
+	$f = $objPHPExcel->getActiveSheet()->getCell("F".$j)->getValue();
+	$g = $objPHPExcel->getActiveSheet()->getCell("G".$j)->getValue();
+	$h = $objPHPExcel->getActiveSheet()->getCell("H".$j)->getValue();
+	$i = $objPHPExcel->getActiveSheet()->getCell("I".$j)->getValue();
+	$wj = $objPHPExcel->getActiveSheet()->getCell("J".$j)->getValue();
+	$k = $objPHPExcel->getActiveSheet()->getCell("K".$j)->getValue();
+	$l = $objPHPExcel->getActiveSheet()->getCell("L".$j)->getValue();
+	$id = $j;
+	
 	// $e = $objPHPExcel->getActiveSheet()->getCell("E".$j)->getValue();
 	// $f = $objPHPExcel->getActiveSheet()->getCell("F".$j)->getValue();
-	$sql = "INSERT INTO `test`(`Id`, `price`, `stock`, `num`) VALUES ('".$a."','".$b."','".$c."','".$d."')";
-
-	// mysqli_query($link,$sql);
-	// mysqli_query($link,'set names utf-8');
+	//$sql = "INSERT INTO `kebiao` VALUES ('".$id."','".$a."','".$b."','".$c."','".$d."','".$e."','".$f."','".$g."','".$h."','".$i."','".$wj."','".$k."','".$l."')";
+	$sql = "INSERT INTO `kebiao` VALUES (null,'".$a."','".$b."','".$c."','".$d."','".$e."','".$f."','".$g."','".$h."','".$i."','".$wj."','".$k."','".$l."')";
+//print_r($sql);die;
+	 //mysqli_query($link,$sql);
+	 //mysqli_query($link,'set names utf-8');
 	if(mysqli_query($link,$sql)){
-		echo 1;
+		echo "成功";
 	}
 	else{
 		echo "导入数据失败";
